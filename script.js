ce4a63d0ec81321c3dba8309c9e4ddc9dc9eef7a
PriyaBalaDharshini <125110897+PriyaBalaDharshini@users.noreply.github.com>
2023-04-15 11:13:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,29 +15,29 @@ function lable_creation(tagname,attriname,attrivalue,content){
     return element;
  }
  var firstname=lable_creation("lable","for","Firstname","Firstname");
- var br1=linebreaker();
+ var br1=linebreaker("br");
  var firstname_input=input_value("input","type","Firstname","id","Firstname");
- var br2=linebreaker()
+ var br2=linebreaker("br")
 
  var middlename=lable_creation("lable","for","Middlename","Middlename");
- var br3=linebreaker();
+ var br3=linebreaker("br");
  var middlename_input=input_value("input","type","Middlename","id","Middlename");
- var br4=linebreaker()
+ var br4=linebreaker("br")
 
  var lastname=lable_creation("lable","for","Lastaname","Lastaname");
- var br5=linebreaker();
+ var br5=linebreaker("br");
  var lastname_input=input_value("input","type","Lastaname","id","Lastaname");
- var br6=linebreaker()
+ var br6=linebreaker("br")
 
  var email=lable_creation("lable","for","email","email");
- var br7=linebreaker();
+ var br7=linebreaker("br");
  var email_input=input_value("input","type","email","id","email");
- var br8=linebreaker()
+ var br8=linebreaker("br")
 
  var password=lable_creation("lable","for","password","password");
- var br9=linebreaker();
+ var br9=linebreaker("br");
  var password_input=input_value("input","type","password","id","password");
- var br10=linebreaker()
+ var br10=linebreaker("br")
  document.body.append(firstname,br1,firstname_input,br2);
  document.body.append(middlename,br3,middlename_input,br4);
  document.body.append(lastname,br5,lastname_input,br6);
